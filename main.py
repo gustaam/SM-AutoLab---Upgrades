12935693cbfd648b4c9a0652997383887c59fcc6
gustaam <gustaam@icloud.com>
2026-08-17 21:32:30 -0300

    Wire v2.66 patch into application startup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from splash import run_splash
 from interface import App
+from patch_v266 import aplicar_patch
 
 
 if __name__ == "__main__":
+    aplicar_patch(App)
     run_splash()
     app = App()
     app.app.mainloop()
