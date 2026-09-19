f0cbcf6755276b3a16cab0368fa000eb60b13587
gustaam <gustaam@icloud.com>
2026-09-19 19:38:45 -0300

    Ajustar testes de armazenamento consolidado

--- tests/test_storage_safe.py
+++ tests/test_storage_safe.py
@@ -3,7 +3,7 @@ import tempfile
 import unittest
 from pathlib import Path
 
-from storage_safe import atomic_write_json, atomic_write_text, backup_path, read_json_with_backup
+from config import atomic_write_json, atomic_write_text, backup_path, read_json_with_backup
 
 
 class StorageSafeTests(unittest.TestCase):
